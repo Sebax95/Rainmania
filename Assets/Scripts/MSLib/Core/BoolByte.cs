3824bc6903a5553781502afc2d05a66e30a74140
Manuelsi <16804326+Manuelsi@users.noreply.github.com>
2020-03-26 17:53:35 -0300

    MSLib added
    Mi libreria personal agregada

---
+++ Assets/Scripts/MSLib/Core/BoolByte.cs
@@ -0,0 +1,76 @@
+﻿using System;
+//using UnityEngine;
+
+namespace CustomMSLibrary {
+
+	namespace Core {
+		/// <summary>
+		/// Structure containing 8 boolean values stored inside a byte.
+		/// Intended to optimize the use of boolean values in memory at the cost of processing.
+		/// </summary>
+		public struct BoolByte {
+			private byte data;
+
+			public byte RawData { get => data; }
+
+			/// <summary>
+			/// Create new BoolByte package from boolean values.
+			/// </summary>
+			/// <param name="data">Boolean values.</param>
+			public BoolByte(params bool[] data) {
+				int length;
+				if (data.Length > 8)
+				{
+					length = 8;
+					throw new ArgumentException("BoolByte created was provided with more than 8 values. Values with index 8 and above ignored.");
+				} else
+					length = data.Length;
+				this.data = 0;
+				for (int i = 0; i < length; i++)
+				{
+					this.data = this.data.SetBit(i, data[i]);
+				}
+			}
+
+			/// <summary>
+			/// Create new BoolByte package from a preexisting byte.
+			/// </summary>
+			/// <param name="data">Byte value.</param>
+			public BoolByte(byte data) {
+				this.data = data;
+			}
+
+			public bool this[int i] {
+				get
+				{
+					if (i < 0 || i > 7) throw new System.IndexOutOfRangeException();
+					return data.GetBit(i);
+				}
+				set
+				{
+					if (i < 0 || i > 7) throw new System.IndexOutOfRangeException();
+					data = data.SetBit(i, value);
+				}
+			}
+
+			#region Overrides
+			public override bool Equals(object obj)
+			{
+				return (data.Equals(obj) && (obj.GetType() == typeof(BoolByte)));
+			}
+
+			public override int GetHashCode()
+			{
+				return (((data.GetHashCode() + 14388) * 3)-2155)/4;
+			}
+			#endregion
+
+			#region Operators
+			public static bool operator ==(BoolByte l, BoolByte r) => l.data == r.data;
+			public static bool operator !=(BoolByte l, BoolByte r) => l.data != r.data;
+			#endregion
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/MSLib/Editor/BakeLightsShortcut.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace CustomMSLibrary.Unity.Editor {
+	public static class LightingUtilities {
+
+		public static bool CanBake => (!Lightmapping.isRunning) &&
+			(Lightmapping.giWorkflowMode == Lightmapping.GIWorkflowMode.OnDemand);
+
+		[MenuItem("Custom Tools/MS Tools/Lighting/Bake Lights", false, 0)]
+		public static void BakeLights() {
+			if(Lightmapping.isRunning)
+				return;
+
+			Lightmapping.BakeAsync();
+		}
+
+		[MenuItem("Custom Tools/MS Tools/Lighting/Bake Lights", true, 0)]
+		public static bool CanBakeValidate() => CanBake;
+
+		//------------------
+
+		[MenuItem("Custom Tools/MS Tools/Lighting/Stop baking", false, 1)]
+		public static void StopBake() {
+			if(!Lightmapping.isRunning)
+				return;
+
+			Lightmapping.Cancel();
+			if(Lightmapping.isRunning)
+				Lightmapping.ForceStop();
+		}
+
+		[MenuItem("Custom Tools/MS Tools/Lighting/Stop baking", true, 1)]
+		public static bool StopBakeValidate() => Lightmapping.isRunning;
+
+		//------------------
+
+		[MenuItem("Custom Tools/MS Tools/Lighting/Toggle Auto-Generate", false, 2)]
+		public static void ToggleAutoBake() {
+			if(Lightmapping.giWorkflowMode == Lightmapping.GIWorkflowMode.OnDemand)
+			{
+				Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.Iterative;
+				Debug.Log("Baking mode set to Auto-Generate");
+			} else
+			{
+				Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
+				Debug.Log("Baking mode set to Manual");
+			}
+		}
+
+
+	}
+}
+
