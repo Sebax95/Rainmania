24eef83aa516429112b6857bc1a409fa368d4609
Manuelsi <16804326+Manuelsi@users.noreply.github.com>
2020-03-26 22:35:18 -0300

    Comienzos de MVC
    Agregadas clases: Character, Controller, ControllerHandler, GameManager (vacio). Agregado archivo contenedor de interfaces.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomMSLibrary.Core;
+
+public abstract class Character : MonoBehaviour
+{
+    public abstract void DoUpdate(Vector3 direction, BoolByte buttons);
+}
