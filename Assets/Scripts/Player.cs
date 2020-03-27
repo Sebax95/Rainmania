88083e76eacf8fdfcb9ad930ee1446c26f5961a1
Manuelsi <16804326+Manuelsi@users.noreply.github.com>
2020-03-26 21:50:14 -0300

    Refactorizacion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using CustomMSLibrary.Core;
 using CustomMSLibrary.Unity;
@@ -13,24 +14,26 @@ public class Player : MonoBehaviour {
 	private Rigidbody rb;
 	private Animator anim;
 	public Vector3 movement;
-	bool canMove;
+	private bool canMove;
 
 	//Attack
 	public GameObject[] attacks;
 	public GameObject arrow;
+	public float whipDelay;
+	public float whipDuration;
 
-	public float timer;
-	bool isAttacking;
-	bool isAttackingUp;
-	bool isAttackingDiag;
+	//public float timer;
+	public float attackCooldown;
+	private WaitForSeconds wait_attackCooldown;
+	private bool canAttack = true;
 
-	public Material weaponColor;
-	private MeshRenderer weaponMesh;
+	//public Material weaponColor;
+	public MeshRenderer weaponMesh;
 	public Material[] weaponMaterials;
-	bool changeWeapon;
+	private bool weaponIsWhip;
 
 	private bool isTryingJump;
-	
+
 
 	private void Start() {
 		// weaponChange.SetColor("_EmissionColor",Color.red );
@@ -41,7 +44,10 @@ public class Player : MonoBehaviour {
 		canMove = true;
 		rb = GetComponent<Rigidbody>();
 		anim = GetComponent<Animator>();
+
+		wait_attackCooldown = new WaitForSeconds(attackCooldown);
 	}
+
 	private void FixedUpdate() {
 		if(rb.velocity.y < 0)
 			rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
@@ -59,12 +65,12 @@ public class Player : MonoBehaviour {
 		Movement();
 
 		if(Input.GetKeyDown(KeyCode.K))
-			changeWeapon = !changeWeapon;
+			weaponIsWhip = !weaponIsWhip;
 
-		if(changeWeapon)
-			weaponColor.SetColor("_EmissionColor", Color.blue);
+		if(weaponIsWhip)
+			weaponMesh.material = weaponMaterials[0];
 		else
-			weaponColor.SetColor("_EmissionColor", Color.red);
+			weaponMesh.material = weaponMaterials[1];
 
 		WhipInput();
 	}
@@ -78,102 +84,131 @@ public class Player : MonoBehaviour {
 		}
 	}
 
-	void Movement() {
+	private void Movement() {
 		anim.SetFloat("SpeedX", movement.x);
 		movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
 		rb.velocity = (new Vector3(movement.x * speed, rb.velocity.y, 0));
 	}
 
-	void WhipInput() {
-		if(Input.GetKey(KeyCode.W))
-		{
-			if(Input.GetAxis("Horizontal") != 0)
-			{
-				if(Input.GetKeyDown(KeyCode.J))
-				{
-					isAttackingUp = false;
-					isAttacking = false;
-					isAttackingDiag = true;
-				}
-			} else if(Input.GetKeyDown(KeyCode.J))
-			{
-				isAttackingUp = true;
-				isAttacking = false;
-				isAttackingDiag = false;
-			}
-		} else if(Input.GetKeyDown(KeyCode.J))
-		{
-			isAttackingUp = false;
-			isAttacking = true;
-			isAttackingDiag = false;
+	private void WhipInput() {
+		bool triedAttack = Input.GetKeyDown(KeyCode.J);
+		(bool isWhip, bool horizontal, bool up) attackDirection =
+			(weaponIsWhip, (Input.GetAxisRaw("Horizontal") != 0), (Input.GetAxisRaw("Vertical") > 0));
 
+		#region deprec
+		//if(Input.GetKey(KeyCode.W))
+		//{
+		//	if(Input.GetAxis("Horizontal") != 0)
+		//	{
+		//		if(triedAttack)
+		//		{
+		//			isAttackingUp = false;
+		//			isAttacking = false;
+		//			isAttackingDiag = true;
+		//		}
+		//	} else if(triedAttack)
+		//	{
+		//		isAttackingUp = true;
+		//		isAttacking = false;
+		//		isAttackingDiag = false;
+		//	}
+		//} else if(triedAttack)
+		//{
+		//	isAttackingUp = false;
+		//	isAttacking = true;
+		//	isAttackingDiag = false;
+		//}
+		#endregion
+
+		if(triedAttack && canAttack)
+		{
+			switch(attackDirection)
+			{
+				//Order: isWhip, horizontal, vertical
+				//whip
+				case var t when t == (true, false, false):
+					WhipAttack(attacks[0]);
+					break;
+				case var t when t == (true, true, false):
+					WhipAttack(attacks[0]);
+					break;
+				case var t when t == (true, false, true):
+					WhipAttack(attacks[1]);
+					break;
+				case var t when t == (true, true, true):
+					WhipAttack(attacks[2]);
+					break;
+
+				//bow
+				case var t when t == (false, false, false):
+					BowNormal();
+					break;
+				case var t when t == (false, true, false):
+					BowNormal();
+					break;
+				case var t when t == (false, false, true):
+					BowUp();
+					break;
+				case var t when t == (false, true, true):
+					BowDiag();
+					break;
+			}
+			StartCoroutine(Coroutine_AttackCooldown());
 		}
-		if(changeWeapon)
-		{
-			if(isAttacking)
-				WhipNormal();
-			if(isAttackingUp)
-				WhipUp();
-			if(isAttackingDiag)
-				WhipDiag();
-		} else
-		{
-			if(isAttacking)
-			{
-				BowNormal();
-				isAttacking = false;
-			}
-			if(isAttackingUp)
-			{
-				BowUp();
-				isAttackingUp = false;
-			}
-			if(isAttackingDiag)
-			{
-				BowDiag();
-				isAttackingDiag = false;
-			}
-		}
 
-
-		if(timer > 0.7f)
-		{
-			isAttacking = false;
-			isAttackingUp = false;
-			isAttackingDiag = false;
-
-			timer = 0;
-		}
 	}
 
-	void WhipNormal() {
-		timer += 1 * Time.deltaTime;
-		if(timer >= 0.4f && timer < 0.6f)
-			attacks[0].SetActive(true);
-		else if(timer > 0.6f)
-		{
-			attacks[0].SetActive(false);
-		}
+	private IEnumerator Coroutine_AttackCooldown() {
+		canAttack = false;
+		yield return wait_attackCooldown;
+		canAttack = true;
 	}
 
-	void WhipUp() {
-		timer += 1 * Time.deltaTime;
-		if(timer >= 0.4f && timer < 0.6f)
-			attacks[1].SetActive(true);
-		else if(timer > 0.6f)
-		{
-			attacks[1].SetActive(false);
-		}
+	private IEnumerator Coroutine_ObjectActiveBlinker(GameObject item, float duration) {
+		item.SetActive(true);
+		yield return new WaitForSeconds(duration);
+		item.SetActive(false);
 	}
-	void WhipDiag() {
-		timer += 1 * Time.deltaTime;
-		if(timer >= 0.4f && timer < 0.6f)
-			attacks[2].SetActive(true);
-		else if(timer > 0.6f)
-		{
-			attacks[2].SetActive(false);
-		}
+
+	private IEnumerator Coroutine_DelayedObjectActiveBlinker(GameObject item, float firstDuration, float secondDuration) {
+		yield return new WaitForSeconds(firstDuration);
+		yield return StartCoroutine(Coroutine_ObjectActiveBlinker(item, secondDuration));
 	}
+
+	private void WhipAttack(GameObject item) {
+		StartCoroutine(Coroutine_DelayedObjectActiveBlinker(item, whipDelay, whipDuration));
+	}
+
+	#region depec
+	//void WhipNormal() {
+	//	timer += 1 * Time.deltaTime;
+	//	if(timer >= 0.4f && timer < 0.6f)
+	//		attacks[0].SetActive(true);
+	//	else if(timer > 0.6f)
+	//	{
+	//		attacks[0].SetActive(false);
+	//	}
+	//}
+
+	//void WhipUp() {
+	//	timer += 1 * Time.deltaTime;
+	//	if(timer >= 0.4f && timer < 0.6f)
+	//		attacks[1].SetActive(true);
+	//	else if(timer > 0.6f)
+	//	{
+	//		attacks[1].SetActive(false);
+	//	}
+	//}
+	//void WhipDiag() {
+	//	timer += 1 * Time.deltaTime;
+	//	if(timer >= 0.4f && timer < 0.6f)
+	//		attacks[2].SetActive(true);
+	//	else if(timer > 0.6f)
+	//	{
+	//		attacks[2].SetActive(false);
+	//	}
+	//}
+	#endregion
 
 	void BowNormal() {
 		var _arrow = Instantiate(arrow);
