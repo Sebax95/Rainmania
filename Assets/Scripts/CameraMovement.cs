6075c6778f761a48984f53c8348a0dea8189accb
AgustinValente <Alcaf123>
2020-03-28 13:10:04 -0300

    Revert "Merge to ManuelBranch master"
    This reverts commit 5b696b050b1f36c63a41d676a92c95524b29a16a, reversing
    changes made to 9036e025426c2a1fd02abe29e756d655c4fc1c72.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Vector3 offset;
+    public Transform baseCamera;
     public Transform player;
     public float smoothMovement = 7;
 
     public void Start()
     {
-        transform.position = player.position + offset;
+        transform.position = baseCamera.position;
     }
 
     private void FixedUpdate()
     {
-        var smooth = Vector3.Slerp(transform.position, player.position + offset, Time.deltaTime * smoothMovement);
+        var smooth = Vector3.Slerp(transform.position, baseCamera.position, Time.deltaTime * smoothMovement);
         transform.position = smooth;
         //transform.LookAt(player);
     }
