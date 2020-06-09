ba48433a4fb6485a0980400138ca606bd219ecc6
Manuelsi <16804326+Manuelsi@users.noreply.github.com>
2020-06-09 14:01:37 -0300

    Trigger cambiador de nivel

---
+++ Assets/Scripts/Misc/LevelChangeTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangeTrigger : MonoBehaviour {
+	public int levelIndex;
+
+	private void OnTriggerEnter(Collider other) {
+		if(!other.gameObject.GetComponent<Player>())
+			return;
+		SceneManager.LoadScene(levelIndex);
+	}
+
+#if UNITY_EDITOR
+	private BoxCollider collidr;
+	private void OnDrawGizmos() {
+		if(!collidr)
+			collidr = GetComponent<BoxCollider>();
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawWireCube(transform.position + collidr.center, collidr.size);
+	}
+#endif
+}
