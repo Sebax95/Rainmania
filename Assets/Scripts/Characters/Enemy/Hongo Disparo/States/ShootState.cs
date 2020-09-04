b5544545061b29e84976f7358381f5b1b4d8ba7e
Sebax95 <sebastian.orrillo@davinci.edu.ar>
2020-09-04 16:11:21 -0300

    Fixes

--- Assets/Scripts/Characters/Enemy/Hongo Disparo/States/ShootState.cs
+++ Assets/Scripts/Characters/Enemy/Hongo Disparo/States/ShootState.cs
@@ -6,17 +6,17 @@ using CustomMSLibrary.Unity;
 public class ShootState : State<MushroomEnemy>
 {
     public ShootState(MushroomEnemy owner, FSM<MushroomEnemy> fsm) : base(owner, fsm) { }
-    public override void Enter()
-    {
-        _owner.canShoot = true;
-    }
+    public override void Enter() { }
     public override void Exit() { }
     public override void FixedUpdateState() { }
     public override void UpdateState()
     {
         if (_owner.LineOfSight())
-           _owner.Shoot();
-        else
+        {
+            if (_owner.canShoot)
+                _owner.Shoot();
+        }
+        else 
             _fsm.SetState(StatesEnemies.Idle);
     }
 }
