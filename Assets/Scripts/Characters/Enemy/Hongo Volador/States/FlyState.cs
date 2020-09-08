376fa67ce6b0eb084538bd965a337a623ff2054a
Sebax95 <sebastian.orrillo@davinci.edu.ar>
2020-09-08 02:58:43 -0300

    Fixes Enemy

--- Assets/Scripts/Characters/Enemy/Hongo Volador/States/FlyState.cs
+++ Assets/Scripts/Characters/Enemy/Hongo Volador/States/FlyState.cs
@@ -19,9 +19,12 @@ public class FlyState : State<FlyingEnemy>
 
     public override void FixedUpdateState()
     {
-     
-        _move = new Vector3(_owner.transform.position.x * 2 * _owner.speed * Time.fixedDeltaTime,
-            Mathf.Sin( (Time.time + rand)  * Mathf.PI * _owner.frecuency) * _owner.amplitud, 0);
+
+        _move = (Vector3.up * Mathf.Sin((Time.time) * Mathf.PI * _owner.frecuency) * _owner.amplitud);
+        _move = _owner.transform.forward *2 * _owner.speed;
+
+        /* _move = new Vector3(_owner.transform.position.x * 2 * _owner.speed * Time.fixedDeltaTime,
+             Mathf.Sin( (Time.time + rand)  * Mathf.PI * _owner.frecuency) * _owner.amplitud, 0);*/
         _owner.Move(_move);
     }
 
