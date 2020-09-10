af20ba6960fcdc0811898cdef5fda66126eca685
Manuelsi <16804326+Manuelsi@users.noreply.github.com>
2020-09-09 21:12:12 -0300

    Crouching
    + un ordenamiento de scripts

---
+++ Assets/Scripts/Characters/Player/CrouchStateRouter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class CrouchStateRouter : MonoBehaviour {
+
+	public bool IsCrouched { get; set; }
+
+	[SerializeField]
+	public PlayerStateParameters standing;
+	[SerializeField]
+	public PlayerStateParameters crouched;
+
+	public PlayerStateParameters Current {
+		get {
+			if(!IsCrouched)
+				return standing;
+			else
+				return crouched;
+		}
+	}
+}
+
+[Serializable]
+public struct PlayerStateParameters {
+	public BoxCastParams[] whipHitbox;
+	public Transform[] arrowSpawns;
+}
--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using UnityEngine;
 using CustomMSLibrary.Core;
 using CustomMSLibrary.Unity;
 
 public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, IAddableVelocity {
+	#region Variables/Properties
 	private const float GROUNDED_DISTANCE = 1.1f;
 
+	//Mobility
 	public float speed;
 	public float forceJump;
 	public float fallMultiplier = 2.5f;
@@ -15,17 +15,26 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 	public LayerMask validFloorLayers;
 	private bool holdingJump = false;
 
+	//Crouched mobility
+	private bool crouched = false;
+	[Range(0f, 1f)]
+	public float crouchSpeedModifier;
+	public LayerMask crouchCheckLayerMask;
+
+	//Grounded
 	public float groundedTreshold;
 	public Vector3 groundCheckOffset;
 	private const int GROUND_FRAMES_PERIOD = 5;
 	private int groundedFramesCounter;
 	public bool Grounded { get; private set; }
 
-
+	//References
 	private MomentumKeeper momentum;
-
 	public PlayerAnim PlayerAnimator { get; private set; }
+	private CrouchStateRouter croucher;
+	private (CapsuleCollider standing, CapsuleCollider crouching, Bounds standChecker) colliders;
 
+	//Attack
 	private bool canAttack = true;
 	private float currentCooldown;
 	private float attackCooldownTimer = 0;
@@ -33,15 +42,12 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 	private Weapon activeWeapon;
 	private LoopingList<Weapon> weapons = new LoopingList<Weapon>();
 
+	//Controller
 	public Controller thisControllerPrefab; //temp
 	private IMoveOverride overriding;
+	#endregion
 
-	//agregueyo mati.
-	//bool canMove = true;
-	////collider salto.
-	//public CapsuleCollider legsCollider;
-	//public CapsuleCollider bodyUpCollider;
-
+	#region Monobehaviour
 	private void Awake() {
 		ControllerHandler.Instance.RequestAssignation(Instantiate(thisControllerPrefab), this);
 	}
@@ -54,12 +60,21 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 		//canMove = true;
 		isDead = false;
 		PlayerAnimator = GetComponent<PlayerAnim>();
+		croucher = GetComponent<CrouchStateRouter>();
+
+		var cols = GetComponents<CapsuleCollider>();
+		colliders.standing = cols[0];
+		colliders.crouching = cols[1];
+		InitHeightChecker();
+		cols[1].enabled = false;
+
 		//Temp, despues ver como SOLIDear asignacion de controller
 		Health = maxHealth;
 
 
 		rb = GetComponent<Rigidbody>();
 		PlayerAnimator = GetComponent<PlayerAnim>();
+		InitCollisionMask();
 	}
 	private void Update() {
 		if(isDead)
@@ -68,10 +83,13 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 		AttackTimer();
 
 	}
+	#endregion
 
+	#region Movement
 	public void Jump() {
 		if(Grounded)
 		{
+			ToggleCrouch(false);
 
 			rb.velocity = rb.velocity.ZeroY();
 			PlayerAnimator.TriggerAction(0);
@@ -85,33 +103,24 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 	}
 
 	public void ForceJump() {
-		//if(canMove)
-		//{
-			rb.AddForce(Vector3.up * forceJump, ForceMode.VelocityChange);
-			PlayerAnimator.Jump();
-			//playerAnimator.thisAnimator.SetBool("inGround", false);
-			holdingJump = true;
-			//bodyUpCollider.enabled = true;
-			//legsCollider.enabled = false;
+		rb.AddForce(Vector3.up * forceJump, ForceMode.VelocityChange);
+		PlayerAnimator.Jump();
+		holdingJump = true;
 
-
-		//}
 
 	}
 	public void ReleaseJump() => holdingJump = false;
 
 	public override void Move(Vector2 direction) {
 
-		if(isDead || overriding != null )
+		if(isDead || overriding != null)
 			return;
+		float mult = 1;
+		if(crouched)
+			mult *= crouchSpeedModifier;
 
-		//if(!canMove)
-		//	return;
-
-		//rb.velocity = new Vector3(direction.x * speed, rb.velocity.y, 0);
 		var vel = rb.velocity;
-		//vel.y -= addedVelocity.y;
-		Vector3 newVel = new Vector3(direction.x * speed, vel.y) + addedVelocity.ZeroY();
+		Vector3 newVel = new Vector3(direction.x * speed * mult, vel.y) + addedVelocity.ZeroY();
 
 		rb.velocity = newVel + momentum.velocity.ZeroY();
 
@@ -119,13 +128,47 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 
 	}
 
+	public void DetectGround() {
+		groundedFramesCounter++;
+		if(!(groundedFramesCounter > GROUND_FRAMES_PERIOD))
+			return;
+		groundedFramesCounter = 0;
+		Grounded = Physics.Raycast(transform.position + groundCheckOffset, Vector3.down, out _, groundedTreshold, validFloorLayers);
+	}
+
+	public void ToggleCrouch(bool state) {
+		if(!state)
+		{
+			var col = colliders.standing as CapsuleCollider;
+			if(!col)
+				throw new ArgumentNullException("Oi, shit-ass. You've changed the collider type, didn't you? Ya fucked up. Not really. Just update the code here to compensate.");
+
+			Bounds checkBounds = colliders.standChecker;
+			bool solidAboveMe = Physics.CheckBox(transform.position + checkBounds.center, checkBounds.extents, transform.rotation, crouchCheckLayerMask);
+			if(solidAboveMe)
+				return;
+		}
+		crouched = state;
+		croucher.IsCrouched = state;
+		colliders.standing.enabled = !state;
+		colliders.crouching.enabled = state;
+		PlayerAnimator.SetCrouched(state);
+	}
+
+	public void ToggleCrouch() =>
+		ToggleCrouch(!crouched);
+	#endregion
+
+	#region Attacking
 	public void Attack(Vector2 direction) {
 		if(!canAttack || isDead)
 			return;
 
-		//canMove = false;
-		//rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
 		SetCooldown();
+		//hack de crouch
+		if(crouched)
+			direction = transform.forward;
+
 		activeWeapon.Attack(direction);
 		PlayerAnimator.Attack(direction, activeWeapon.Name);
 	}
@@ -142,6 +185,29 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 		activeWeapon = weapons[id];
 	}
 
+	public string ActiveWeaponName => activeWeapon.Name;
+
+	private void AttackTimer() {
+		if(canAttack)
+			return;
+
+		attackCooldownTimer += Time.deltaTime;
+		if(attackCooldownTimer > currentCooldown)
+		{
+			//canMove = true;
+			canAttack = true;
+		}
+	}
+
+	private void SetCooldown() {
+		canAttack = false;
+		attackCooldownTimer = 0;
+		currentCooldown = activeWeapon.FullAttackDuration;
+
+	}
+	#endregion
+
+	#region Health
 	public override void Damage(int amount, IDamager source) {
 		base.Damage(amount, source);
 		PlayerAnimator.Hurt();
@@ -166,7 +232,9 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 		GameManager.Instance.PlayerDie();
 		Destroy(gameObject, 3);
 	}
+	#endregion
 
+	#region Misc
 	public void Attach(IMoveOverride controller) {
 		overriding = controller;
 	}
@@ -175,40 +243,45 @@ public class Player : Character, IWielder, IMoveOverrideable, IAppliableForce, I
 		overriding = null;
 	}
 
-	public void DetectGround() {
-		groundedFramesCounter++;
-		if(!(groundedFramesCounter > GROUND_FRAMES_PERIOD))
-			return;
-		groundedFramesCounter = 0;
-		Grounded = Physics.Raycast(transform.position + groundCheckOffset, Vector3.down, out _, groundedTreshold, validFloorLayers);
-	}
-
-	public string ActiveWeaponName => activeWeapon.Name;
-
-	private void AttackTimer() {
-		if(canAttack)
-			return;
-
-		attackCooldownTimer += Time.deltaTime;
-		if(attackCooldownTimer > currentCooldown)
-		{
-			//canMove = true;
-			canAttack = true;
-		}
-	}
-
-	private void SetCooldown() {
-		canAttack = false;
-		attackCooldownTimer = 0;
-		currentCooldown = activeWeapon.FullAttackDuration;
-
-	}
-
 	public void ApplyForce(Vector3 direction, ForceMode mode) {
 		rb.velocity = new Vector3(rb.velocity.x, 0);
 		rb.AddForce(direction, mode);
 		PlayerAnimator.TriggerAction(0);
 	}
 
+	private void InitHeightChecker() {
+		var standCol = colliders.standing;
+		var crouchCol = colliders.crouching;
+
+		Vector3 center = standCol.center;
+		float standTrueHeight = standCol.height + standCol.radius;
+		float crouchTrueHeight = crouchCol.height + crouchCol.radius;
+
+		float deltaHeight = standTrueHeight - crouchTrueHeight;
+		float floorHeight = center.y - (standTrueHeight / 2f);
+		float newCenterHeight = floorHeight + crouchTrueHeight + (deltaHeight / 2f);
+
+		float radius = colliders.standing.radius;
+		Vector3 newCenter = new Vector3(center.x, newCenterHeight, center.z);
+		colliders.standChecker = new Bounds() {
+			center = newCenter,
+			extents = Vector3.one * radius
+		};
+	}
+
+	private void InitCollisionMask() {
+		int myLayer = gameObject.layer;
+		int layerMask = 0;
+
+		for(int i = 0; i < 32; i++)
+		{
+			if(!Physics.GetIgnoreLayerCollision(myLayer, i))
+			{
+				layerMask = layerMask | (1 << i);
+			}
+		}
+		crouchCheckLayerMask = layerMask;
+	}
+	#endregion
 
 }
