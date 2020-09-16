f895fda4df75e18a6f19aa9ee3d4ef73d4bad7a7
AgustinValente <Alcaf123>
2020-09-16 19:26:14 -0300

    Menu + Characater
    El menu con opciones ya esta, hay que cambiarle el skin a los botones
    El personaje con las animaciones ya esta subido. (Assets/ NewAssets/ Character)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject controls;
+    public GameObject credits;
+    bool _true = false;
+    bool _true2 = false;
+    void Start()
+    {
+        controls.SetActive(false);
+        credits.SetActive(false);
+    }
+
+
+    private void OnMouseDown()
+    {
+        Play();
+        Controls();
+        Credits();
+        Exit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Controls()
+    {
+        _true =! _true;
+        controls.SetActive(_true);
+    }
+
+    public void Credits()
+    {
+        _true2 =! _true2;
+        credits.SetActive(_true2);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
