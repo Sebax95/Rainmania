86dee2c2a44dfa41a1b63dc5840bf030cb2a974a
AgustinValente <Alcaf123>
2020-09-17 16:37:55 -0300

    Level Selector (Main Menu)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,38 +8,66 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject controls;
     public GameObject credits;
+    public GameObject levels;
     bool _true = false;
     bool _true2 = false;
+    bool _true3 = false;
     void Start()
     {
         controls.SetActive(false);
         credits.SetActive(false);
+        levels.SetActive(false);
     }
 
 
-    private void OnMouseDown()
+    /*private void OnMouseDown()
     {
         Play();
         Controls();
         Credits();
         Exit();
-    }
+    }*/
 
     public void Play()
     {
         SceneManager.LoadScene(1);
     }
 
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
     public void Controls()
     {
         _true =! _true;
+        _true3 = false;
         controls.SetActive(_true);
+        levels.SetActive(false);
     }
 
     public void Credits()
     {
         _true2 =! _true2;
+        _true3 = false;
         credits.SetActive(_true2);
+        levels.SetActive(false);
+    }
+
+    public void Levels()
+    {
+        _true = false;
+        _true2 = false;
+        _true3 = !_true3;
+        controls.SetActive(false);
+        credits.SetActive(false);
+        levels.SetActive(_true3);
+
     }
 
     public void Exit()
