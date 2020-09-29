90c816338090e3066d584b7b005853bc015250ec
Sebax95 <49926152+Sebax95@users.noreply.github.com>
2020-09-29 17:30:15 -0300

    Revert "Merge branch 'master' into EnemigoV-olador"
    This reverts commit 36294acedbe161f930e6ccdb2fa9450c396bfd54, reversing
    changes made to ffd1ae2866dec639e58580b690bce2eff0c83781.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -43,11 +43,6 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(3);
     }
 
-<<<<<<< HEAD
-=======
-    public void Level4() => SceneManager.LoadScene(4);
-
->>>>>>> master
     public void Controls()
     {
         _true =! _true;
