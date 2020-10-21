53ff9418ea3e22f30d9d02504f7427aacb9927b7
Manuelsi <16804326+Manuelsi@users.noreply.github.com>
2020-10-21 19:49:46 -0300

    Upgrade system wip. Before rework

---
+++ Assets/Scripts/Data/UpgradesData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Upgrade Data Preset", menuName = "Upgrade Data Preset", order = 4)]
+public class UpgradesData : ScriptableObject {
+	[Header("General")]
+	public int playerMaxLife;
+
+	[Header("Bow")]
+	public bool bowAcquired = false;
+	public bool arrowCanPlatform = false;
+	public bool arrowCanAnchor;
+	public int arrowMaxAmount;
+	public int arrowDamage;
+	public float arrowShootSpeed;
+
+	[Header("Whip")]
+	public bool whipAcquired = false;
+	public bool whipCanGrapple = false;
+	public int whipDamage;
+	public float whipAttackSpeed;
+
+	public void IncrementIntProperty(ref int property, int amount) => property += amount;
+	public void DecrementFloatProperty(ref float property, float amount) => property -= amount;
+	public void MultiplyFloatProperty(ref float property, float mult) => property *= mult;
+	public void EnableBoolProperty(ref bool property) => property = true;
+}
