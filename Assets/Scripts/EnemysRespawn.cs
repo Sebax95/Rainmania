f61d8c0f2567ccbe469a2a63d6f89d861d5cbabd
AgustinValente <Alcaf123>
2020-10-21 20:28:31 -0300

    Enemy Respawn
    Solo la base, falta la funcion para que los enemigos se reseteen

---
+++ Assets/Scripts/EnemysRespawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemysRespawn : MonoBehaviour
+{
+    public Enemy[] enemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                //enemy[i].Reset;
+            }
+        }
+    }
+}
