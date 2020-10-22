25cde33e8d68ee5644fd095731b3678858b40b21
Manuelsi <16804326+Manuelsi@users.noreply.github.com>
2020-10-22 00:24:07 -0300

    Upgrade system wip. Arrays de nombre-valor

--- Assets/Scripts/Data/UpgradesData.cs
+++ Assets/Scripts/Data/UpgradesData.cs
@@ -1,28 +1,78 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
 [CreateAssetMenu(fileName = "Upgrade Data Preset", menuName = "Upgrade Data Preset", order = 4)]
 public class UpgradesData : ScriptableObject {
-	[Header("General")]
-	public int playerMaxLife;
 
-	[Header("Bow")]
-	public bool bowAcquired = false;
-	public bool arrowCanPlatform = false;
-	public bool arrowCanAnchor;
-	public int arrowMaxAmount;
-	public int arrowDamage;
-	public float arrowShootSpeed;
-
-	[Header("Whip")]
-	public bool whipAcquired = false;
-	public bool whipCanGrapple = false;
-	public int whipDamage;
-	public float whipAttackSpeed;
-
-	public void IncrementIntProperty(ref int property, int amount) => property += amount;
-	public void DecrementFloatProperty(ref float property, float amount) => property -= amount;
-	public void MultiplyFloatProperty(ref float property, float mult) => property *= mult;
-	public void EnableBoolProperty(ref bool property) => property = true;
+	public NameIntPair[] intProperties;
+	public NameFloatPair[] floatProperties;
+	public NameBoolPair[] boolProperties;
+	
+	public void IncrementIntProperty(string propertyName, int amount) => intProperties[GetIntIndex(propertyName)].value  += amount;
+	public void DecrementFloatProperty(string propertyName, float amount) => floatProperties[GetIntIndex(propertyName)].value -= amount;
+	public void MultiplyFloatProperty(string propertyName, float mult) => floatProperties[GetIntIndex(propertyName)].value *= mult;
+	public void EnableBoolProperty(string propertyName) => boolProperties[GetIntIndex(propertyName)].value = true;
+	
+	#region Seeker Funcs
+	private int GetIntIndex(string name) {
+		int index = -1;
+		int size = intProperties.Length;
+		for(int i = 0; i < size; i++)
+		{
+			if(intProperties[i].name == name)
+			{
+				index = i;
+				return index;
+			}
+		}
+		throw new KeyNotFoundException("Given property name does not exist.");
+	}
+	private int GetFloatIndex(string name) {
+		int index = -1;
+		int size = floatProperties.Length;
+		for(int i = 0; i < size; i++)
+		{
+			if(floatProperties[i].name == name)
+			{
+				index = i;
+				return index;
+			}
+		}
+		throw new KeyNotFoundException("Given property name does not exist.");
+	}
+	private int GetBoolIndex(string name) {
+		int index = -1;
+		int size = boolProperties.Length;
+		for(int i = 0; i < size; i++)
+		{
+			if(boolProperties[i].name == name)
+			{
+				index = i;
+				return index;
+			}
+		}
+		throw new KeyNotFoundException("Given property name does not exist.");
+	}
+	#endregion
 }
+
+//Por que no tuplas? Porque no se pueden serializar
+[Serializable]
+public class NameIntPair {
+	public string name;
+	public int value;
+}
+[Serializable]
+public class NameFloatPair {
+	public string name;
+	public float value;
+}
+[Serializable]
+public class NameBoolPair {
+	public string name;
+	public bool value;
+}
+
--- Assets/Scripts/Managers/UpgradesManager.cs
+++ Assets/Scripts/Managers/UpgradesManager.cs
@@ -14,11 +14,6 @@ public class UpgradesManager : MonoBehaviour {
 		DontDestroyOnLoad(this);
 	}
 
-	public void IncrementIntProperty(ref int property, int amount) => data.IncrementIntProperty(ref property, amount);
-	public void DecrementFloatProperty(ref float property, float amount) => data.DecrementFloatProperty(ref property, amount);
-	public void MultiplyFloatProperty(ref float property, float mult) => data.MultiplyFloatProperty(ref property, mult);
-	public void EnableBoolProperty(ref bool property) => data.EnableBoolProperty(ref property);
-
 	public string SerializeData() => JsonUtility.ToJson(data);
 	public void SaveDataFromJson(string data) => JsonUtility.FromJsonOverwrite(data, this.data);
 }
