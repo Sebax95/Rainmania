84803686369b978044f250be6a12500e9e785bd6
AgustinValente <Alcaf123>
2020-11-05 15:57:13 -0300

    Almost

--- Assets/Scripts/Levels Trigers (Agus)/Doors.cs
+++ Assets/Scripts/Levels Trigers (Agus)/Doors.cs
@@ -8,24 +8,31 @@ public class Doors : MonoBehaviour
     Player player;
     Camera _camera;
     bool _canUse;
+    public int _lock;
+    int _key = 0;
 
     private void Awake()
     {
         player = FindObjectOfType<Player>();
        _camera = FindObjectOfType<Camera>();
+       // EventsManager.SubscribeToEvent(EventType.GP_KeyTipe,KeyType);
     }
 
     void Update()
     {
         if(_canUse)
-            if(Input.GetKeyDown(KeyCode.E))
+            if(Input.GetKeyDown(KeyCode.E) && _key <= _lock)
             {
                 player.transform.position = _otherDoor.position;
                 _camera.transform.position = new Vector3(_otherDoor.transform.position.x, _otherDoor.transform.position.y, _otherDoor.transform.position.z - 6);
             }
     }
 
-   
+   //void KeyType(params object[] parameters)
+   //{
+   //     _key = (int)parameters[0];
+   //}
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
