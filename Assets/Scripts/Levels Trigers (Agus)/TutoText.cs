f68774903adc6efe9376242d71d6175285f12487
AgustinValente <Alcaf123>
2020-11-22 16:00:38 -0300

    TEmp

---
+++ Assets/Scripts/Levels Trigers (Agus)/TutoText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoText : MonoBehaviour
+{
+    public GameObject _text;
+ 
+    void Start()
+    {
+        _text.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+  
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            _text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _text.SetActive(false);
+        }
+    }
+}
